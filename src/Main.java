4cce3d1e7d422cb50e6591add90bdbe340c2450a
Robert Szekely <szekelyrobert1995@gmail.com>
2016-02-27 12:24:53 +0200

    3rd commit :)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello from the other side!");
+        System.out.println("I think I'm starting to understand how this works....");
     }
 }
