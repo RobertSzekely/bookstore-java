b38dce52d6e674d42b0c8c3db0b73aa131eb7259
Robert Szekely <szekelyrobert1995@gmail.com>
2016-03-06 12:31:18 +0200

    modified main

--- src/main/java/ro/ubb/bookstore/Main.java
+++ src/main/java/ro/ubb/bookstore/Main.java
@@ -12,13 +12,6 @@ import java.util.Scanner;
 public class Main {
 
 
-
-//   public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//        System.out.println("Hello from the other side!");
-//        System.out.println("I think I'm starting to understand how this works....");
-
-
     public static void main(String [] args) {
 
         Scanner input  = new Scanner(System.in);
