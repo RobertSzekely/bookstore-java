0d3763e312e00146896212f490d5b9860561658b
Robert Szekely <szekelyrobert1995@gmail.com>
2016-03-06 13:10:09 +0200

    adding a book to the memory repository working :)

--- src/main/java/ro/ubb/bookstore/ui/Console.java
+++ src/main/java/ro/ubb/bookstore/ui/Console.java
@@ -1,8 +1,15 @@
 package main.java.ro.ubb.bookstore.ui;
 
 import main.java.ro.ubb.bookstore.controller.BookController;
+import main.java.ro.ubb.bookstore.domain.Book;
+import main.java.ro.ubb.bookstore.domain.Validators.ValidatorException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Created by robertszekely on 27/02/16.
@@ -15,13 +22,16 @@ public class Console {
     }
 
     public void runConsole() {
-        printMainMenu();
+        //printMainMenu();
+        //addBook();
+        //printAllBooks();
+        mainMenu();
     }
 
 
     Scanner input  = new Scanner(System.in).useDelimiter("\\n");;
 
-    public void printMainMenu() {
+    public void printMainMenuOptions() {
         System.out.println("\nOptions for books:\n"
                 + "(1) Print all books\n"
                 + "(2) Add a book\n"
@@ -42,6 +52,59 @@ public class Console {
         );
 
     }
+
+    public void printAllBooks() {
+        Set<Book> books = bookController.getAllBooks();
+        books.stream().forEach(System.out::println);
+    }
+
+    private void addBooks() {
+        while(true) {
+            Book book = readBook();
+            if(book == null || book.getId() < 0) {
+                break;
+            }
+            try {
+                bookController.addBook(book);
+            } catch (ValidatorException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void addBook() {
+        Book book = readBook();
+        try {
+            bookController.addBook(book);
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private Book readBook() {
+        System.out.println("Read book {id, ISBN, title, author, price, units");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            Long id = Long.valueOf(bufferedReader.readLine());
+            String ISBN = bufferedReader.readLine();
+            String title = bufferedReader.readLine();
+            String author = bufferedReader.readLine();
+            Float price = Float.valueOf(bufferedReader.readLine());
+            int units = Integer.parseInt(bufferedReader.readLine());
+
+            Book book = new Book(ISBN, title, author, price, units);
+            book.setId(id);
+
+            return book;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+
+    }
+
+
 
     public void printAddNewBook() {
         System.out.print("\nEnter ISBN: ");
@@ -95,4 +158,77 @@ public class Console {
         String lastname = input.next();
         // TODO: 27/02/16
     }
+
+
+    private void mainMenu() {
+        int opt;
+
+        do {
+            printMainMenuOptions();
+            boolean cont = true;
+            opt = 0;
+            do {
+                System.out.print("\nEnter option: ");
+                opt = input.nextInt();
+                cont = false;
+            } while (cont == true);
+
+            switch(opt) {
+                case 1:
+                    printAllBooks();
+                    break;
+                case 2:
+                    addBook();
+                    break;
+                case 3:
+                    printDeleteBook();
+                    //// TODO: 27/02/16
+                    break;
+                case 4:
+                    printUpdateBook();
+                    // TODO: 27/02/16
+                    break;
+                case 5:
+                    // TODO: 27/02/16
+                    break;
+                case 6:
+                    // TODO: 27/02/16
+                case 7:
+                    // TODO: 27/02/16
+                    break;
+                case 8:
+                    // TODO: 27/02/16
+                    break;
+                case 9:
+                    // TODO: 27/02/16
+                    break;
+                case 10:
+                    printAddNewClient();
+                    // TODO: 27/02/16
+                    break;
+                case 11:
+                    printDeleteClient();
+                    // TODO: 27/02/16
+                    break;
+                case 12:
+                    printUpdateClient();
+                    // TODO: 27/02/16
+                    break;
+                case 15:
+                    // TODO: 27/02/16
+                    break;
+                case 14:
+                    // TODO: 27/02/16
+                    break;
+                default:
+                    break;
+            }
+        } while(opt != 0);
+
+
+        System.out.println("\nExecution stopped");
+        input.close();
+
+    }
+
 }
