e763cfba71c09afe88ba700d7d307b2bd5193c91
Robert Szekely <szekelyrobert1995@gmail.com>
2016-03-13 16:20:59 +0200

    removed isbn from book, added category (getters and setters)

--- src/main/java/ro/ubb/bookstore/domain/Book.java
+++ src/main/java/ro/ubb/bookstore/domain/Book.java
@@ -4,7 +4,6 @@ package main.java.ro.ubb.bookstore.domain;
  * Created by robertszekely on 27/02/16.
  */
 public class Book extends BaseEntity<Long> {
-    private String ISBN;
     private String title;
     private String author;
     private String category;
@@ -15,10 +14,10 @@ public class Book extends BaseEntity<Long> {
     //toodo Modifications on branch develop
 
     //Constructor (id is automatically assigned to each book)
-    public Book(String _ISBN, String _title, String _author, float _price) {
-        this.ISBN = _ISBN;
+    public Book(String _title, String _author, String _category, float _price) {
         this.title = _title;
         this.author = _author;
+        this.category = _category;
         this.price = _price;
     }
 
@@ -26,9 +25,6 @@ public class Book extends BaseEntity<Long> {
     //commit me.
     /*-------------------Getters--------------------------*/
 
-    public String getISBN() {
-        return this.ISBN;
-    }
 
     public String getTitle() {
         return title;
@@ -49,10 +45,6 @@ public class Book extends BaseEntity<Long> {
 
     /*---------------------Setters----------------------------*/
 
-    public void setISBN(String new_ISBN) {
-        this.ISBN = new_ISBN;
-    }
-
     public void setTitle(String new_title) {
         this.title = new_title;
     }
@@ -69,7 +61,6 @@ public class Book extends BaseEntity<Long> {
         this.price = new_price;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -77,9 +68,9 @@ public class Book extends BaseEntity<Long> {
 
         Book book = (Book) o;
 
-        if(ISBN != book.ISBN) return false;
         if(title != book.title) return false;
         if(author != book.author) return false;
+        if(category != book.category) return false;
         if(price != book.price) return false;
         return title.equals(book.title);
 
@@ -88,9 +79,9 @@ public class Book extends BaseEntity<Long> {
     @Override
     public String toString() {
         return "Book{" +
-                "ISBN='" + ISBN + '\'' +
-                ", title='" + title + '\'' +
+                "title='" + title + '\'' +
                 ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
                 ", price='" + price +
                 "} " + super.toString();
     }
